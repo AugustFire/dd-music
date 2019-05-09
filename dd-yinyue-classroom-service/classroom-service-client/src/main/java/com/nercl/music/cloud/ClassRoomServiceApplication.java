0c500667cf209eca6cab84ee14c6e468709518fe
yzx <1694645918@qq.com>
2019-05-09 08:51:19 +0800

    修改配置,提升数据库版本

--- dd-yinyue-classroom-service/classroom-service-client/src/main/java/com/nercl/music/cloud/ClassRoomServiceApplication.java
+++ dd-yinyue-classroom-service/classroom-service-client/src/main/java/com/nercl/music/cloud/ClassRoomServiceApplication.java
@@ -2,19 +2,13 @@ package com.nercl.music.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ClassRoomServiceApplication extends SpringBootServletInitializer {
+public class ClassRoomServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ClassRoomServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ClassRoomServiceApplication.class, args);
+    }
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(ClassRoomServiceApplication.class);
-	}
 
 }
--- dd-yinyue-exercise-service/src/main/java/com/nercl/music/cloud/ExerciseServiceApplication.java
+++ dd-yinyue-exercise-service/src/main/java/com/nercl/music/cloud/ExerciseServiceApplication.java
@@ -2,19 +2,13 @@ package com.nercl.music.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ExerciseServiceApplication extends SpringBootServletInitializer {
+public class ExerciseServiceApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExerciseServiceApplication.class, args);
 	}
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(ExerciseServiceApplication.class);
-	}
 
 }
